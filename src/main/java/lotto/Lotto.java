77834de9065e3efdbd1d9c9de3afb4b1b0060c95
david-parkk <ji0513ji@naver.com>
2025-11-03 15:42:01 +0900

    feat: 로또 클래스 추가
    - 에러 케이스 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,10 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+    private final String ERROR_LOG = "[ERROR] ";
+    private final String INVALID_LOTTO_LENGTH = "로또 번호는 6개여야 합니다.";
+    private final String DUPLICATE_LOTTO_NUMBER = "로또 번호는 중복값이 허용되지 않습니다.";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -12,9 +16,15 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            System.out.println(ERROR_LOG + INVALID_LOTTO_LENGTH);
+            throw new IllegalArgumentException(ERROR_LOG + INVALID_LOTTO_LENGTH);
+        }
+        int count = (int) numbers.stream()
+                .distinct()
+                .count();
+        if (count != numbers.size()) {
+            System.out.println(ERROR_LOG + DUPLICATE_LOTTO_NUMBER);
+            throw new IllegalArgumentException(ERROR_LOG + DUPLICATE_LOTTO_NUMBER);
         }
     }
-
-    // TODO: 추가 기능 구현
 }
