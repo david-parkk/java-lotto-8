c0e04edc1b0a236736488138504f285ea1906a5b
david-parkk <ji0513ji@naver.com>
2025-11-03 18:59:20 +0900

    feat: 로또 팩터리 메서드 추가
    - 로또, 보너스 번호 주입

--- src/main/java/lotto/LottoFactory.java
+++ src/main/java/lotto/LottoFactory.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class LottoFactory {
+    private final String ERROR_LOG = "[ERROR] ";
+    private final String DUPLICATE_BONUS_NUMBER = "보너스 번호는 로또 번호와 중복이 허용되지 않습니다.";
 
     private RandomCreator randomCreator;
 
@@ -13,6 +15,10 @@ public class LottoFactory {
 
     public Lotto createRandomLotto(int lottoLength) {
         List<Integer> numbers = randomCreator.createRandomNumbers(lottoLength);
+        return new Lotto(numbers);
+    }
+
+    public Lotto createLotto(List<Integer> numbers) {
         return new Lotto(numbers);
     }
 
@@ -34,5 +40,13 @@ public class LottoFactory {
             }
             return new BonusLotto(randomNumber);
         }
+    }
+
+    public BonusLotto createBonusLotto(Lotto lotto, int bonusNumber) {
+        if (lotto.getNumbers().contains(bonusNumber)) {
+            System.out.println(ERROR_LOG + DUPLICATE_BONUS_NUMBER);
+            throw new IllegalArgumentException(ERROR_LOG + DUPLICATE_BONUS_NUMBER);
+        }
+        return new BonusLotto(bonusNumber);
     }
 }
