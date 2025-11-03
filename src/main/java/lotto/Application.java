acd4d852f8ae728aca422d071c22442c84270954
david-parkk <ji0513ji@naver.com>
2025-11-03 20:14:53 +0900

    feat: 메인 클래스 수 정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,21 @@
 package lotto;
 
 import java.util.List;
+import lotto.io.Input;
 
 public class Application {
     public static void main(String[] args) {
+        Input input = new Input();
         LottoFactory lottoFactory = new LottoFactory(1, 60);
-        Lotto lotto = lottoFactory.createRandomLotto(6);
-        BonusLotto bonusLotto = lottoFactory.createRandomBonusLotto(lotto);
+
+        int money = input.inputMoney();
+
+        List<Integer> lottoNumbers = input.inputLottoNumber();
+        Lotto lotto = lottoFactory.createLotto(lottoNumbers);
+
+        int bonusNumber = input.inputBonusNumber();
+
+        BonusLotto bonusLotto = lottoFactory.createBonusLotto(lotto, bonusNumber);
 
         WinningFactory winningFactory = new WinningFactory();
         List<WinningType> winningTypes = winningFactory.createWinningTypes();
