3ce3873d903a5f2a7bfea1877165e15389ea671d
david-parkk <ji0513ji@naver.com>
2025-11-03 18:03:40 +0900

    feat: 메인 함수 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,7 +5,13 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         LottoFactory lottoFactory = new LottoFactory(1, 60, 6);
+        Lotto lotto = lottoFactory.createRandomLotto();
+        BonusLotto bonusLotto = lottoFactory.createRandomBonusLotto(lotto);
+
         WinningFactory winningFactory = new WinningFactory();
         List<WinningType> winningTypes = winningFactory.createWinningTypes();
+        WinningMachine winningMachine = new WinningMachine(winningTypes);
+
+        LottoMachine lottoMachine = new LottoMachine(lotto, bonusLotto, winningMachine);
     }
 }
